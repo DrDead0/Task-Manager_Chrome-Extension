c957949e17b6c4b132e0ba9023fa2545a6b996b2
Ashish Chaurasiya <ashishchaurasiya128@gmail.com>
2024-06-20 11:41:30 +0530

    Fixed bug

--- background.js
+++ background.js
@@ -1,16 +1,17 @@
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     if (message.action === "createAlarm") {
-      const dueDate = new Date(message.dueDate); 
+      const dueDate = new Date(message.dueDate);
       chrome.alarms.create(message.title, { when: dueDate.getTime() });
     }
   });
   
-  chrome.alarms.onAlarm.addListener(function(alarm) {
+  chrome.alarms.onAlarm.addListener(function (alarm) {
     chrome.notifications.create({
       type: "basic",
-      iconUrl: "icon.png", 
+      iconUrl: chrome.runtime.getURL("icon.png"),
       title: "Task Reminder",
-      message: alarm.name
+      message: alarm.name,
     });
   });
+  
   
