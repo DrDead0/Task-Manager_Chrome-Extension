4e89302d7a34ff6dff6b4f0b3fe8d992ba0c9147
DrDead0 <112349103+DrDead0@users.noreply.github.com>
2024-11-02 02:32:37 +0530

    bug fix

--- background.js
+++ background.js
@@ -1,17 +1,50 @@
+// // chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+// //     if (message.action === "createAlarm") {
+// //       const dueDate = new Date(message.dueDate);
+// //       chrome.alarms.create(message.title, { when: dueDate.getTime() });
+// //     }
+// //   });
+  
+// //   chrome.alarms.onAlarm.addListener(function (alarm) {
+// //     chrome.notifications.create({
+// //       type: "basic",
+// //       iconUrl: chrome.runtime.getURL("icon.png"),
+// //       title: "Task Reminder",
+// //       message: alarm.name,
+// //     });
+// //   });
+  
+  
+//   chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+//     if (message.action === "createAlarm") {
+//         const dueDate = new Date(message.dueDate);
+//         chrome.alarms.create(message.title, { when: dueDate.getTime() });
+//     }
+// });
+
+// chrome.alarms.onAlarm.addListener(function (alarm) {
+//     chrome.notifications.create({
+//         type: "basic",
+//         iconUrl: chrome.runtime.getURL("icon.png"),
+//         title: "Task Reminder",
+//         message: alarm.name,
+//     });
+// });
+
+// background.js
+
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     if (message.action === "createAlarm") {
-      const dueDate = new Date(message.dueDate);
-      chrome.alarms.create(message.title, { when: dueDate.getTime() });
+        const dueDate = new Date(message.dueDate);
+        chrome.alarms.create(message.title, { when: dueDate.getTime() });
     }
-  });
-  
-  chrome.alarms.onAlarm.addListener(function (alarm) {
+});
+
+chrome.alarms.onAlarm.addListener(function (alarm) {
     chrome.notifications.create({
-      type: "basic",
-      iconUrl: chrome.runtime.getURL("icon.png"),
-      title: "Task Reminder",
-      message: alarm.name,
+        type: "basic",
+        iconUrl: chrome.runtime.getURL("icon.png"),
+        title: "Task Reminder",
+        message: alarm.name,
     });
-  });
-  
-  
+});
